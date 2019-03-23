195e2bc5d69b5b45e26a60f8f2731f0e896f31f1
polinafilacheva <polina.filacheva@gmail.com>
2019-03-23 17:01:45 +0300

    новая концепция

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
-// anime({
-//   targets: ".box",
-//   right: 100,
-//   duration: 3000
-// })
+anime({
+  targets: '.box',
+  translateX: {
+    value: 0.3,
+    duration: 800
+
+  },
+  rotate: {
+    value: 1440,
+    duration: 6500,
+    easing: 'easeInOutSine',
+  },
+  scale: {
+    value: 1,
+    duration: 1600,
+    delay: 800,
+    easing: 'easeInOutQuart'
+  },
+  delay: 250
+});
+
+anime({
+  targets: '#new',
+  translateX: {
+    value: 0.3,
+    duration: 800
+  },
+  rotate: {
+    value: 1440,
+    duration: 6500,
+    easing: 'easeInOutSine'
+  },
+  scale: {
+    value: 1,
+    duration: 1600,
+    delay: 800,
+    easing: 'easeInOutQuart'
+  },
+  delay: 250
+});
